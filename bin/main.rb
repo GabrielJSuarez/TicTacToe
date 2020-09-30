1669c4d5620cf7271a461867d50c1f70c2a7ed28
Gabriel Suárez <ginnlaiho@gmail.com>
2020-09-30 12:11:16 -0500

    add board to the console

--- bin/main.rb
+++ bin/main.rb
@@ -3,60 +3,63 @@
 #Board
 
 board = [
-  [[], [], []],
-  [[], [], []],
-  [[], [], []]
+  [["x"], ["-"], ["-"]],
+  [["-"], ["-"], ["-"]],
+  [["-"], ["-"], ["0"]]
 ]
 
-#while something
+puts board.map {|x| x.join("|")}
 
-  #PLAYER ONE INPUT
-  puts "player 1 choose position: "
-  puts "player 1 choose line"
-  player_1_row = gets.chomp
-  puts "player 1 choose column"
-  player_1_column = gets.chomp
 
-  #MODIFYING THE BOARD WITH PLAYER 1 INPUT
-  board[player_1_row.to_i][player_1_column.to_i] = "x"
+# #while something
 
-  #RE-PRINT THE BOARD WITH THE NEW VALUE
-  puts board[0]
-  puts board[1]
-  puts board[2]
+#   #PLAYER ONE INPUT
+#   puts "player 1 choose position: "
+#   puts "player 1 choose line"
+#   player_1_row = gets.chomp
+#   puts "player 1 choose column"
+#   player_1_column = gets.chomp
+
+#   #MODIFYING THE BOARD WITH PLAYER 1 INPUT
+#   board[player_1_row.to_i][player_1_column.to_i] = "x"
+
+#   #RE-PRINT THE BOARD WITH THE NEW VALUE
+#   puts board[0]
+#   puts board[1]
+#   puts board[2]
 
   
 
-  #CHANGE OF PLAYERS
+#   #CHANGE OF PLAYERS
   
-  puts "player 2 choose position: "
-  puts "player 2 choose line"
-  player_2_row = gets.chomp
-  puts "player 2 choose column"
-  player_2_column = gets.chomp
-
-  #MODIFYING THE BOARD WITH PLAYER 1 INPUT
-  board[player_2_row.to_i][player_2_column.to_i] = "o"
-
-  #RE-PRINT THE BOARD WITH THE NEW VALUE
-  puts board[0]
-  puts board[1]
-  puts board[2]
-
-
-#   begin
-#     #if there is a value in the position
-#   rescue StandardError => e
-#     puts "#{e.name}"    
-#   end
-#     #display the board with the X in the choice
-
 #   puts "player 2 choose position: "
-#   player_2 = gets.chomp
-#   begin
-#     #if there is a value in the position
-#   rescue StandardError => e
-#     puts "#{e.name}"    
-#   end
-#     #display the board with the O in the choice
-# #end of loop
+#   puts "player 2 choose line"
+#   player_2_row = gets.chomp
+#   puts "player 2 choose column"
+#   player_2_column = gets.chomp
+
+#   #MODIFYING THE BOARD WITH PLAYER 1 INPUT
+#   board[player_2_row.to_i][player_2_column.to_i] = "o"
+
+#   #RE-PRINT THE BOARD WITH THE NEW VALUE
+#   puts board[0]
+#   puts board[1]
+#   puts board[2]
+
+
+# #   begin
+# #     #if there is a value in the position
+# #   rescue StandardError => e
+# #     puts "#{e.name}"    
+# #   end
+# #     #display the board with the X in the choice
+
+# #   puts "player 2 choose position: "
+# #   player_2 = gets.chomp
+# #   begin
+# #     #if there is a value in the position
+# #   rescue StandardError => e
+# #     puts "#{e.name}"    
+# #   end
+# #     #display the board with the O in the choice
+# # #end of loop
