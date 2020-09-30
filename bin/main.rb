d8562b48769b67927251fc3cb4af3e0e4125a6d8
Gabriel Suárez <ginnlaiho@gmail.com>
2020-09-30 14:36:41 -0500

    Add loop and conditions for each turn

--- bin/main.rb
+++ bin/main.rb
@@ -13,67 +13,44 @@ puts board.map {|x| x.join(" | ")}
 count_free_space = 9
 
 
-while count_free_space >= 0
+while count_free_space >= 1
 
-  #PLAYER ONE INPUT
-  puts "player 1 choose position: "
-  puts "player 1 choose line"
-  player_1_row = gets.chomp
-  puts "player 1 choose column"
-  player_1_column = gets.chomp
+    # Step 1 - Get player input
+    puts "player 1 choose position: "
+    puts "player 1 choose line"
+    player_1_row = gets.chomp
+    puts "player 1 choose column"
+    player_1_column = gets.chomp
+
+    # Stpe 2 - MODIFYING THE BOARD WITH PLAYER 1 INPUT
+    if board[player_1_row.to_i][player_1_column.to_i] == "-"
+        board[player_1_row.to_i][player_1_column.to_i] = "x"
+        count_free_space -= 1
+    else 
+      p "space already fill"
+    end
+
+    #RE-PRINT THE BOARD WITH THE NEW VALUE
+    puts board.map {|x| x.join(" | ")}
+    
+    
+    #CHANGE OF PLAYERS
+    puts "player 2 choose position: "
+    puts "player 2 choose line"
+    player_2_row = gets.chomp
+    puts "player 2 choose column"
+    player_2_column = gets.chomp
+
+    #MODIFYING THE BOARD WITH PLAYER 2 INPUT
+
+    if board[player_2_row.to_i][player_2_column.to_i] == "-" 
+        board[player_2_row.to_i][player_2_column.to_i] = "o"
+        count_free_space -= 1
+    else 
+      p "space already fill"
+    end
 
 
-  #MODIFYING THE BOARD WITH PLAYER 1 INPUT
-  # board[player_1_row.to_i][player_1_column.to_i] = "x"
-  if board[player_1_row.to_i][player_1_column.to_i] == "-"
-      board[player_1_row.to_i][player_1_column.to_i] = "x"
-      count_free_space -= 1
-  else 
-    p "space already fill"
-  end
-
-  #RE-PRINT THE BOARD WITH THE NEW VALUE
-  puts board.map {|x| x.join(" | ")}
-  # your move has been display
-
-
-  #CHANGE OF PLAYERS
-  
-  puts "player 2 choose position: "
-  puts "player 2 choose line"
-  player_2_row = gets.chomp
-  puts "player 2 choose column"
-  player_2_column = gets.chomp
-
-  #MODIFYING THE BOARD WITH PLAYER 1 INPUT
-
-  # now it will swap values
-  if board[player_2_row.to_i][player_2_column.to_i] == "-" 
-      board[player_2_row.to_i][player_2_column.to_i] = "o"
-      count_free_space -= 1
-  else 
-    p "space already fill"
-  end
-
-  #RE-PRINT THE BOARD WITH THE NEW VALUE
-  puts board.map {|x| x.join(" | ")}
-  # your move has been display
-
+    #RE-PRINT THE BOARD WITH THE NEW VALUE
+    puts board.map {|x| x.join(" | ")}
 end
-
-# #   begin
-# #     #if there is a value in the position
-# #   rescue StandardError => e
-# #     puts "#{e.name}"    
-# #   end
-# #     #display the board with the X in the choice
-
-# #   puts "player 2 choose position: "
-# #   player_2 = gets.chomp
-# #   begin
-# #     #if there is a value in the position
-# #   rescue StandardError => e
-# #     puts "#{e.name}"    
-# #   end
-# #     #display the board with the O in the choice
-# # #end of loop
