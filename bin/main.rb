3aaa0b3ad52e60af63f8058f85f0900a057c5597
ricardo <ramn21@gmail.com>
2020-09-30 22:45:10 +0200

    add comments with step of the game

--- bin/main.rb
+++ bin/main.rb
@@ -14,43 +14,54 @@ count_free_space = 9
 
 while count_free_space >= 1
 
-  # Step 1 - Get player 1 input
+  # Step 1 - GET PLAYER 1 INPUT
   puts "player 1 choose position: "
   puts "player 1 choose line"
   player_1_row = gets.chomp
+  # Step 2 - CHECK IF ITS VALID INPUT
   puts "player 1 choose column"
   player_1_column = gets.chomp
+  # Step 3 - CHECK IF ITS VALID INPUT
 
-  # Step 2 - MODIFYING THE BOARD WITH PLAYER 1 INPUT
+  # Step 4 - MODIFYING THE BOARD WITH PLAYER 1 INPUT
   if board[player_1_row.to_i][player_1_column.to_i] == "-"
     board[player_1_row.to_i][player_1_column.to_i] = "x"
     count_free_space -= 1
   else 
+    #Step 5 - IF POSITION IS NOT VALID ASK FOR NEW POSITION
     p "space already fill"
   end
-
-  # Step 3 - RE-PRINT THE BOARD WITH THE NEW VALUE
+  
+  # Step 6 - RE-PRINT THE BOARD WITH THE NEW VALUE
   puts board.map {|x| x.join(" | ")}
+  # STEP 7 -CHECK IF PLAYER 1 IS THE WINNER, IF PLAYER 1 WIN RETURN RESULT, GAME END
   
   #CHANGE OF PLAYERS
 
-  # Step 4 - Get player input
+  # Step 7 - GET PLAYER 2 INPUT
   puts "player 2 choose position: "
   puts "player 2 choose line"
   player_2_row = gets.chomp
+  # Step 8 - CHECK IF ITS VALID INPUT
   puts "player 2 choose column"
   player_2_column = gets.chomp
+  # Step 9 - CHECK IF ITS VALID INPUT
 
-  # Step 5 - MODIFYING THE BOARD WITH PLAYER 2 INPUT
+  # Step 10 - MODIFYING THE BOARD WITH PLAYER 2 INPUT
   if board[player_2_row.to_i][player_2_column.to_i] == "-" 
     board[player_2_row.to_i][player_2_column.to_i] = "o"
     count_free_space -= 1
   else 
+    #Step 11 - IF POSITION IS NOT VALID ASK FOR NEW POSITION
     p "space already fill"
   end
 
-  # Step 6 - RE-PRINT THE BOARD WITH THE NEW VALUE
+  # Step 12 - RE-PRINT THE BOARD WITH THE NEW VALUE
   puts board.map {|x| x.join(" | ")}
+
+  # STEP 13 -CHECK IF PLAYER 2 IS THE WINNER, IF PLAYER 2 WIN RETURN RESULT, GAME END
+
+  # Step 14 - IF NO PLAYER WINS, MESSAGE GAME IS DRAW, GAME END
 
 end
 
