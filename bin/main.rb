0b561b0d26239f9846215dff60f883ab83257ad9
ricardo <ramn21@gmail.com>
2020-09-30 22:27:12 +0200

    add if statement

--- bin/main.rb
+++ bin/main.rb
@@ -14,30 +14,44 @@ count_free_space = 9
 
 while count_free_space >= 1
 
-    # Step 1 - Get player 1 input
-    puts "player 1 choose position: "
-    puts "player 1 choose line"
-    player_1_row = gets.chomp
-    puts "player 1 choose column"
-    player_1_column = gets.chomp
+  # Step 1 - Get player 1 input
+  puts "player 1 choose position: "
+  puts "player 1 choose line"
+  player_1_row = gets.chomp
+  puts "player 1 choose column"
+  player_1_column = gets.chomp
 
-    # Step 2 - MODIFYING THE BOARD WITH PLAYER 1 INPUT
+  # Step 2 - MODIFYING THE BOARD WITH PLAYER 1 INPUT
+  if board[player_1_row.to_i][player_1_column.to_i] == "-"
+    board[player_1_row.to_i][player_1_column.to_i] = "x"
+    count_free_space -= 1
+  else 
+    p "space already fill"
+  end
 
-    # Step 3 - RE-PRINT THE BOARD WITH THE NEW VALUE
-    puts board.map {|x| x.join(" | ")}
-    
-    #CHANGE OF PLAYERS
+  # Step 3 - RE-PRINT THE BOARD WITH THE NEW VALUE
+  puts board.map {|x| x.join(" | ")}
+  
+  #CHANGE OF PLAYERS
 
-    # Step 4 - Get player input
-    puts "player 2 choose position: "
-    puts "player 2 choose line"
-    player_2_row = gets.chomp
-    puts "player 2 choose column"
-    player_2_column = gets.chomp
+  # Step 4 - Get player input
+  puts "player 2 choose position: "
+  puts "player 2 choose line"
+  player_2_row = gets.chomp
+  puts "player 2 choose column"
+  player_2_column = gets.chomp
 
-    # Step 5 - MODIFYING THE BOARD WITH PLAYER 2 INPUT
+  # Step 5 - MODIFYING THE BOARD WITH PLAYER 2 INPUT
+  if board[player_2_row.to_i][player_2_column.to_i] == "-" 
+    board[player_2_row.to_i][player_2_column.to_i] = "o"
+    count_free_space -= 1
+  else 
+    p "space already fill"
+  end
 
-    # Step 6 - RE-PRINT THE BOARD WITH THE NEW VALUE
-    puts board.map {|x| x.join(" | ")}
+  # Step 6 - RE-PRINT THE BOARD WITH THE NEW VALUE
+  puts board.map {|x| x.join(" | ")}
 
 end
+
+ 
