b8f73bd00d6a786dddbd40ba13e827d43a7529d2
Gabriel Suárez <ginnlaiho@gmail.com>
2020-09-30 12:43:00 -0500

    Add conditions to the user inputs

--- bin/main.rb
+++ bin/main.rb
@@ -3,54 +3,58 @@
 #Board
 
 board = [
-  [["-"], ["-"], ["-"]],
-  [["-"], ["-"], ["-"]],
-  [["-"], ["-"], ["-"]]
+  ["-", "-", "-"],
+  ["-", "-", "-"],
+  ["-", "-", "-"]
 ]
-display = board.map {|x| x.join(" | ")}
-puts  display
+
+puts board.map {|x| x.join(" | ")}
+
+
 
 
 # #while something
 
-#   #PLAYER ONE INPUT
-#   puts "player 1 choose position: "
-#   puts "player 1 choose line"
-#   player_1_row = gets.chomp
-#   puts "player 1 choose column"
-#   player_1_column = gets.chomp
-
-#   #MODIFYING THE BOARD WITH PLAYER 1 INPUT
-#   if board[player_1_row.to_i][player_1_column.to_i] === "-"
-#       board[player_1_row.to_i][player_1_column.to_i] = "x"
-#   else 
-#     error "space already fill"
-#   end
-
-#   #RE-PRINT THE BOARD WITH THE NEW VALUE
-#   puts display
-#   your move has been display
+  #PLAYER ONE INPUT
+  puts "player 1 choose position: "
+  puts "player 1 choose line"
+  player_1_row = gets.chomp
+  puts "player 1 choose column"
+  player_1_column = gets.chomp
 
 
+  #MODIFYING THE BOARD WITH PLAYER 1 INPUT
+  # board[player_1_row.to_i][player_1_column.to_i] = "x"
+  if board[player_1_row.to_i][player_1_column.to_i] == "-"
+      board[player_1_row.to_i][player_1_column.to_i] = "x"
+  else 
+    p "space already fill"
+  end
+
+  #RE-PRINT THE BOARD WITH THE NEW VALUE
+  puts board.map {|x| x.join(" | ")}
+  # your move has been display
+
+
+  #CHANGE OF PLAYERS
   
+  puts "player 2 choose position: "
+  puts "player 2 choose line"
+  player_2_row = gets.chomp
+  puts "player 2 choose column"
+  player_2_column = gets.chomp
 
-#   #CHANGE OF PLAYERS
-  
-#   puts "player 2 choose position: "
-#   puts "player 2 choose line"
-#   player_2_row = gets.chomp
-#   puts "player 2 choose column"
-#   player_2_column = gets.chomp
+  #MODIFYING THE BOARD WITH PLAYER 1 INPUT
+  # board[player_2_row.to_i][player_2_column.to_i] = "o"
+  if board[player_2_row.to_i][player_2_column.to_i] == "-" 
+      board[player_2_row.to_i][player_2_column.to_i] = "o"
+  else 
+    p "space already fill"
+  end
 
-#   #MODIFYING THE BOARD WITH PLAYER 1 INPUT
-#   if board[player_2_row.to_i][player_2_column.to_i] === "-" 
-#       board[player_2_row.to_i][player_2_column.to_i] = "o"
-#   else 
-#     error "space already fill"
-#   end
-#   #RE-PRINT THE BOARD WITH THE NEW VALUE
-#   puts display
-#   your move has been display
+  #RE-PRINT THE BOARD WITH THE NEW VALUE
+  puts board.map {|x| x.join(" | ")}
+  # your move has been display
 
 
 
