26d96ec3854bd1ddc3f956ede0bb478ed44668b6
ricardo <ramn21@gmail.com>
2020-09-30 18:37:53 +0200

    add user interface

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,30 @@
 #!/usr/bin/env ruby
-puts "Hello World!"
+player_1
+#display X to player_1
+player_2
+#display O to player_2
+
+#while something 
+  puts "player 1 choose position: "
+  puts "player 1 choose line"
+  player_1_row = gets.chomp
+  puts "player 1 choose column"
+  player_1_column = gets.chomp
+
+  x_array << player_1_row
+  begin
+    #if there is a value in the position
+  rescue StandardError => e
+    puts "#{e.name}"    
+  end
+    #display the board with the X in the choice
+
+  puts "player 2 choose position: "
+  player_2 = gets.chomp
+  begin
+    #if there is a value in the position
+  rescue StandardError => e
+    puts "#{e.name}"    
+  end
+    #display the board with the O in the choice
+#end of loop
