6557c781b3de0c4f374af512fc925cb95249d15a
Gabriel Suárez <ginnlaiho@gmail.com>
2020-10-01 17:30:21 -0500

    Modify the game logic and calling to the interface

--- bin/main.rb
+++ bin/main.rb
@@ -49,7 +49,7 @@ class UserInput < Display
         puts 'space already fill'
       end
     else
-      puts "not a valid position, try again!"
+      puts "not a valid position, next player turn!"
     end  
   end
 
@@ -58,30 +58,22 @@ end
 class GameLogic < UserInput
 
   attr_accessor :game_status, :moves_left
+
+  @@winning_move_1 = @@board[@player_row.to_i].all?(@simbol)
+
+  @@winning_move_2 = [@@board[0][@player_column.to_i], @@board[1][@player_column.to_i], @@board[2][@player_column.to_i]].all?(@simbol)
+  
+  @@winning_move_3 = [@@board[0][0], @@board[1][1], @@board[2][2]].all?(@simbol)
+  
+  @@winning_move_4 = [@@board[0][2], @@board[1][1], @@board[2][0]].all?(@simbol)
   
   def initialize
     @game_status = true
-    @moves_left = 9
-
-    @winning_move_1 = @@board[@player_row.to_i].all?(@simbol)
-
-    @winning_move_2= [@@board[0][@player_column.to_i], @@board[1][@player_column.to_i], @@board[2][@player_column.to_i]].all?(@simbol)
-  
-    @winning_move_3 = [@@board[0][0], @@board[1][1], @@board[2][2]].all?(@simbol)
-  
-    @winning_move_4 = [@@board[0][2], @@board[1][1], @@board[2][0]].all?(@simbol)
+    @moves_left = 8
   end  
-  
-  # @winning_move_1 = @@board[@player_row.to_i].all?(@simbol)
-
-  # @winning_move_2= [@@board[0][@player_column.to_i], @@board[1][@player_column.to_i], @@board[2][@player_column.to_i]].all?(@simbol)
-
-  # @winning_move_3 = [@@board[0][0], @@board[1][1], @@board[2][2]].all?(@simbol)
-
-  # @winning_move_4 = [@@board[0][2], @@board[1][1], @@board[2][0]].all?(@simbol)
 
   def check_winner()
-    if @winning_move_1 || @winning_move_2 || @winning_move_3 || @winning_move_4
+    if @@winning_move_1 || @@winning_move_2 || @@winning_move_3 || @@winning_move_4
       puts "Winner!"
       @game_status = false
     elsif @moves_left == 0
@@ -89,7 +81,7 @@ class GameLogic < UserInput
       @game_status = false
     else
       puts "Next player's turn"
-      @moves_left -=1
+      @moves_left -= 1
     end
   end
 end
@@ -117,18 +109,18 @@ player_one_input = gets.chomp
 puts "Player two name"
 player_two_input = gets.chomp
 
-
-winner = GameLogic.new
-
 player_one = Display.new(player_one_input, "x")
 player_two = Display.new(player_two_input, "o")
 
 player_one_input = UserInput.new(player_one.name, player_one.symbol)
-
 player_two_input = UserInput.new(player_two.name, player_two.symbol)
 
-while winner.game_status
+game_logic = GameLogic.new
 
+play_game = true
+
+while play_game
+  
   Display.table
 
   player_one_input.ask_input
@@ -136,8 +128,10 @@ while winner.game_status
 
   Display.table
 
-  winner.check_winner()
-  if winner.game_status == false || winner.moves_left == 0
+  game_logic.check_winner
+
+  if game_logic.game_status == false
+    play_game = game_logic.game_status
     break
   end
 
@@ -146,15 +140,12 @@ while winner.game_status
 
   Display.table
 
-  winner.check_winner()
-  if winner.game_status == false || winner.moves_left == 0
+  game_logic.check_winner
+
+  if game_logic.game_status == false
+    play_game = game_logic.game_status
     break
   end
+
 end
 
-
-
-
-# while game_status
-  
-# end
