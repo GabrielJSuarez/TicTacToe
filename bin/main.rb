bbca6662fb026f9e5b02fae5f78db64d09fed878
Gabriel Suárez <ginnlaiho@gmail.com>
2020-10-01 15:13:54 -0500

    create classes to divide display, user input and game logic

--- bin/main.rb
+++ bin/main.rb
@@ -3,113 +3,102 @@
 # Board
 
 class Display
-  def board (board)
-    puts (board.map { |x| x.join(' | ') })
+
+  attr_accessor :board, :users
+
+  @@board = [
+    ['-', '-', '-'],
+    ['-', '-', '-'],
+    ['-', '-', '-']
+  ]
+
+  private
+  def self.board
+    puts (@@board.map { |x| x.join(' | ') })
+  end
+
+  def self.users
+    puts "Enter player's name"
+    player_name = gets.chomp
+    puts "Enter player's simbol"
+    player_simbol = gets.chomp
   end
 end
 
-class Players 
+class UserInput
 
-end
+  attr_reader :simbol
 
-board = [
-  ['-', '-', '-'],
-  ['-', '-', '-'],
-  ['-', '-', '-']
-]
+  def initialize(player, simbol)
+    @player = player
+    @simbol = simbol
+  end
 
-first_board = Display.new
-first_board.board(board)
+  def ask_input()
+    puts "#{@player}, make your move: "
+    puts "#{@player} chooses row"
+    @player_row = gets.chomp 
+    puts "#{@player} chooses column"
+    @player_column = gets.chomp
+  end
 
-# puts(board.map { |x| x.join(' | ') })
-
-game_status = true
-moves_left = 9
-
-while game_status
- 
-
-  puts 'player 1 choose position: '
-  puts 'player 1 choose line'
-  player_1_row = gets.chomp 
-  puts 'player 1 choose column'
-  player_1_column = gets.chomp
-
-  if (player_1_row <= "2" && player_1_row >= "0") && (player_1_column <= "2" && player_1_column >= "0")
-    if board[player_1_row.to_i][player_1_column.to_i] == '-'
-      board[player_1_row.to_i][player_1_column.to_i] = 'x'
-      moves_left -= 1
+  def check_input()
+    if (@player_row <= "2" && @player_row >= "0") && (@player_column <= "2" && @player_column >= "0")
+      if board[@player_row.to_i][@player_column.to_i] == '-'
+        board[@player_row.to_i][@player_column.to_i] = 'x'
+        moves_left -= 1
+      else
+        puts 'space already fill'
+      end
     else
-      puts 'space already fill'
-    end
-  else
-    puts "not a valid position, try again!"
-  end  
-
-  first_board.board(board)
-
-
-
-  winning_move_1_p1 = board[player_1_row.to_i].all?("x")
-
-  winning_move_2_p1 = [board[0][player_1_column.to_i], board[1][player_1_column.to_i], board[2][player_1_column.to_i]].all?("x")
-
-  winning_move_3_p1 = [board[0][0], board[1][1], board[2][2]].all?("x")
-
-  winning_move_4_p1 = [board[0][2], board[1][1], board[2][0]].all?("x")
-
-  if winning_move_1_p1 || winning_move_2_p1 || winning_move_3_p1 || winning_move_4_p1
-    puts "Winner!"
-    game_status = false
-    break
-  elsif moves_left == 0
-    puts "The game is a Draw!"
-    game_status = false
-    break
-  else
-    puts "Next player's turn"
+      puts "not a valid position, try again!"
+    end  
   end
+
+end
+
+# class GameLogic
+
+#   attr_accessor :game_status, :moves_left
+
+#   @@game_status = true
+#   @@moves_left = 9
+
+#   winning_move_1_p1 = board[player_1_row.to_i].all?("x")
+
+#   winning_move_2_p1 = [board[0][player_1_column.to_i], board[1][player_1_column.to_i], board[2][player_1_column.to_i]].all?("x")
+
+#   winning_move_3_p1 = [board[0][0], board[1][1], board[2][2]].all?("x")
+
+#   winning_move_4_p1 = [board[0][2], board[1][1], board[2][0]].all?("x")
+
+#   def check_winner()
+#     if winning_move_1_p1 || winning_move_2_p1 || winning_move_3_p1 || winning_move_4_p1
+#       puts "Winner!"
+#       game_status = false
+#     elsif moves_left == 0
+#       puts "The game is a Draw!"
+#       game_status = false
+#     else
+#       puts "Next player's turn"
+#     end
+#   end
   
+  
+  
+# end
 
-  # CHANGE OF PLAYERS
+player = Display.users
 
-  puts 'player 2 choose position: '
-  puts 'player 2 choose line'
-  player_2_row = gets.chomp
-  puts 'player 2 choose column'
-  player_2_column = gets.chomp
+p player.player_name
+# player_one = UserInput.new(player_name, player_simbol)
+# player_two = UserInput.new(player_name, player_simbol)
 
-  if (player_2_row <= "2" && player_2_row >= "0") && (player_2_column <= "2" && player_2_column >= "0")
-    if board[player_2_row.to_i][player_2_column.to_i] == '-'
-      board[player_2_row.to_i][player_2_column.to_i] = 'o'
-      moves_left -= 1
-    else
-      puts 'space already fill'
-    end
-  else
-    puts "not a valid position, try again!"
-  end  
+# player_one_simbol = player_one.simbol
+# player_two_simbol = player_two.simbol
 
-  puts(board.map { |x| x.join(' | ') })
+Display.board
 
-  winning_move_1_p2 = board[player_2_row.to_i].all?("o")
-
-  winning_move_2_p2 = [board[0][player_2_column.to_i], board[1][player_2_column.to_i], board[2][player_2_column.to_i]].all?("o")
-
-  winning_move_3_p2 = [board[0][0], board[1][1], board[2][2]].all?("o")
-
-  winning_move_4_p2 = [board[0][2], board[1][1], board[2][0]].all?("o")
-
-  if winning_move_1_p2 || winning_move_2_p2 || winning_move_3_p2 || winning_move_4_p2
-    puts "Winner!"
-    game_status = false
-    break
-  elsif moves_left == 0
-    puts "The game is a Draw!"
-    game_status = false
-    break
-  else
-    puts "Next player's turn"
-  end
-
-end
+# while game_status
+  
+# end
