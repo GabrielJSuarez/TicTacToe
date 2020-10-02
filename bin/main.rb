fb85c10358a0dc4e621355e9f3550cfb6d3511c1
Gabriel Suárez <ginnlaiho@gmail.com>
2020-10-02 13:59:10 -0500

    fix bug in input behaviour

--- bin/main.rb
+++ bin/main.rb
@@ -34,22 +34,21 @@ class UserInput < Display
   def ask_input()
     puts "#{@player}, make your move: "
     puts "#{@player} chooses row"
-    @player_row = gets.chomp.to_i
+    @player_row = gets.chomp
     puts "#{@player} chooses column"
-    @player_column = gets.chomp.to_i
+    @player_column = gets.chomp
   end
 
   def check_play()
-    return if (@player_row <= 2 && @player_row >= 0) && (@player_column <= 2 && @player_column >= 0)
-
+    return if (@player_row.to_i <= 3 && @player_row.to_i >= 1 && @player_row.length.positive?) && (@player_column.to_i <= 3 && @player_column.to_i >= 1 && @player_column.length.positive?)
     puts 'not a valid input, try again'
     ask_input
     check_play
   end
 
   def player_move(simbol)
-    if @@board[@player_row][@player_column] == '-'
-      @@board[@player_row][@player_column] = simbol
+    if @@board[@player_row.to_i - 1][@player_column.to_i - 1] == '-'
+      @@board[@player_row.to_i - 1][@player_column.to_i - 1] = simbol
     else
       puts 'space already fill'
       ask_input
@@ -69,9 +68,9 @@ class GameLogic < UserInput
   # rubocop:disable Metrics/CyclomaticComplexity
   # rubocop:disable Metrics/PerceivedComplexity
   def check_winner(row, column, simbol)
-    @winningmove1 = @@board[row].all?(simbol)
+    @winningmove1 = @@board[row.to_i - 1].all?(simbol)
 
-    @winningmove2 = [@@board[0][column], @@board[1][column], @@board[2][column]].all?(simbol)
+    @winningmove2 = [@@board[0][column.to_i - 1], @@board[1][column.to_i - 1], @@board[2][column.to_i - 1]].all?(simbol)
 
     @winningmove3 = [@@board[0][0], @@board[1][1], @@board[2][2]].all?(simbol)
 
