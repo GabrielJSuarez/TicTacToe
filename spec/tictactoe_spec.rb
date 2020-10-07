fa15b06889f1c2338df59f16418a969ff847bde0
Gabriel Suárez <ginnlaiho@gmail.com>
2020-10-07 17:31:10 -0500

    refector code base

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -5,36 +5,24 @@ describe GameLogic do
   subject { GameLogic.new }
 
   describe '#draw?' do
-    context "game drawed" do 
-      let(:draw_positive) do
-        subject.check_draw(board, p_row, p_column, simbol)
-      end
+    context "check draw board" do 
+      let(:draw_positive) {
+        subject.check_draw(draw_board, "1", "1", "o")
+      }
 
-      let(:board) do
+      let(:draw_negative) {
+        subject.check_draw(not_draw_board, "2", "2", "x")
+      }
+
+      let(:draw_board) {
         [
           %w[o x x],
           %w[x o o],
           %w[o x x]
         ]
-      end
+      }
 
-      let(:p_row) { '1' }
-
-      let(:p_column) { '1' }
-
-      let(:simbol) { 'o' }
-
-      example 'draw game' do
-        expect(draw_positive).to be true
-      end
-    end
-    
-    context "not a draw" do
-      let(:draw_negative) do
-        subject.check_draw(board, p_row, p_column, simbol)
-      end
-
-      let(:board) {
+      let(:not_draw_board) {
         [
           %w[- x x],
           %w[x - -],
@@ -42,11 +30,9 @@ describe GameLogic do
         ]
       }
 
-      let(:p_row) { '2' }
-
-      let(:p_column) { '2' }
-
-      let(:simbol) { 'x' }
+      example 'draw game' do
+        expect(draw_positive).to be true
+      end
 
       example 'non draw game' do
         expect(draw_negative).to be false
