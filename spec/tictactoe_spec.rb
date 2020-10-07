2a07441ef1e6a65d358691dd9f44d6d2875e7bf9
Gabriel Suárez <ginnlaiho@gmail.com>
2020-10-07 17:32:39 -0500

    Fix rubocop linter flags

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -5,30 +5,30 @@ describe GameLogic do
   subject { GameLogic.new }
 
   describe '#draw?' do
-    context "check draw board" do 
-      let(:draw_positive) {
-        subject.check_draw(draw_board, "1", "1", "o")
-      }
+    context 'check draw board' do
+      let(:draw_positive) do
+        subject.check_draw(draw_board, '1', '1', 'o')
+      end
 
-      let(:draw_negative) {
-        subject.check_draw(not_draw_board, "2", "2", "x")
-      }
+      let(:draw_negative) do
+        subject.check_draw(not_draw_board, '2', '2', 'x')
+      end
 
-      let(:draw_board) {
+      let(:draw_board) do
         [
           %w[o x x],
           %w[x o o],
           %w[o x x]
         ]
-      }
+      end
 
-      let(:not_draw_board) {
+      let(:not_draw_board) do
         [
           %w[- x x],
           %w[x - -],
           %w[o - x]
         ]
-      }
+      end
 
       example 'draw game' do
         expect(draw_positive).to be true
@@ -40,23 +40,23 @@ describe GameLogic do
     end
   end
 
-  describe "#winner?" do
-    context "p1 won" do
+  describe '#winner?' do
+    context 'p1 won' do
       let(:p1_wins) do
-        subject.check_winner(board, "1", "2", "x")
+        subject.check_winner(board, '1', '2', 'x')
       end
 
       let(:p1_does_not_wins) do
-        subject.check_winner(board, "1", "2", "o")
+        subject.check_winner(board, '1', '2', 'o')
       end
 
-      let(:board) {
+      let(:board) do
         [
           %w[x x x],
           %w[- - -],
           %w[- - -]
         ]
-      }
+      end
 
       example 'p1 wins' do
         expect(p1_wins).to be true
@@ -67,22 +67,22 @@ describe GameLogic do
       end
     end
 
-    context "p2 won" do
+    context 'p2 won' do
       let(:p2_wins) do
-        subject.check_winner(board, "1", "1", "o")
+        subject.check_winner(board, '1', '1', 'o')
       end
 
       let(:p2_does_not_wins) do
-        subject.check_winner(board, "1", "1", "x")
+        subject.check_winner(board, '1', '1', 'x')
       end
 
-      let(:board) {
+      let(:board) do
         [
           %w[o x x],
           %w[o - -],
           %w[o - -]
         ]
-      }
+      end
 
       example 'p2 wins' do
         expect(p2_wins).to be true
