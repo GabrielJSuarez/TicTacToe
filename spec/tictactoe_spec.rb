8d4ae9f199b518a65b4dab4612456b58c33eee36
Gabriel Suárez <ginnlaiho@gmail.com>
2020-10-07 17:09:08 -0500

    Modify sintax

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -4,43 +4,53 @@ require './lib/logic'
 describe GameLogic do
   subject { GameLogic.new }
 
-  context 'draw?' do
-    let(:draw_positive) do
-      subject.check_draw(board, p_row, p_column, simbol)
+  describe '#draw?' do
+    context "game drawed" do 
+      let(:draw_positive) do
+        subject.check_draw(board, p_row, p_column, simbol)
+      end
+
+      let(:board) do
+        [
+          %w[o x x],
+          %w[x o o],
+          %w[o x x]
+        ]
+      end
+
+      let(:p_row) { '1' }
+
+      let(:p_column) { '1' }
+
+      let(:simbol) { 'o' }
+
+      example 'draw game' do
+        expect(draw_positive).to be true
+      end
     end
+    
+    context "not a draw" do
+      let(:draw_negative) do
+        subject.check_draw(board, p_row, p_column, simbol)
+      end
 
-    let(:draw_negative) do
-      subject.check_draw(n_draw_board, p_row, p_column, simbol)
-    end
+      let(:board) {
+        [
+          %w[- x x],
+          %w[x - -],
+          %w[o - x]
+        ]
+      }
 
-    let(:board) do
-      [
-        %w[o x x],
-        %w[x o o],
-        %w[o x x]
-      ]
-    end
+      let(:p_row) { '2' }
 
-    let(:n_draw_board) do
-      [
-        %w[- x x],
-        %w[x - -],
-        %w[o - x]
-      ]
-    end
+      let(:p_column) { '2' }
 
-    let(:p_row) { '1' }
+      let(:simbol) { 'x' }
 
-    let(:p_column) { '1' }
-
-    let(:simbol) { 'o' }
-
-    example 'draw game' do
-      expect(draw_positive).to be true
-    end
-
-    example 'non draw game' do
-      expect(draw_negative).to be false
+      example 'non draw game' do
+        expect(draw_negative).to be false
+      end
     end
   end
 end
