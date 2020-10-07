308b38823344d1ff648945730da7850761b63f8d
Gabriel Suárez <ginnlaiho@gmail.com>
2020-10-07 17:20:08 -0500

    Add basic tests for winner conditions

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -53,4 +53,80 @@ describe GameLogic do
       end
     end
   end
+
+  describe "#winner?" do
+    context "p1 won" do
+      let(:p1_winner) do
+        subject.check_winner(board, p_row, p_column, simbol)
+      end
+
+      let(:board) {
+        [
+          %w[x x x],
+          %w[- - -],
+          %w[- - -]
+        ]
+      }
+
+      let(:p_row) { '1' }
+
+      let(:p_column) { '2' }
+
+      let(:simbol) { 'x' }
+
+      example 'non draw game' do
+        expect(p1_winner).to be true
+      end
+
+      
+    end
+
+    context "p2 won" do
+      let(:p2_winner) do
+        subject.check_winner(board, p_row, p_column, simbol)
+      end
+
+      let(:board) {
+        [
+          %w[o o o],
+          %w[- - -],
+          %w[- - -]
+        ]
+      }
+
+      let(:p_row) { '1' }
+
+      let(:p_column) { '2' }
+
+      let(:simbol) { 'o' }
+
+      example 'non draw game' do
+        expect(p2_winner).to be true
+      end
+    end
+  end
 end
+
+# let(:board_2) {
+#   [
+#     %w[x - -],
+#     %w[x - -],
+#     %w[x - -]
+#   ]
+# }
+
+# let(:board_3) {
+#   [
+#     %w[x - -],
+#     %w[- x -],
+#     %w[- - x]
+#   ]
+# }
+
+# let(:board_4) {
+#   [
+#     %w[- - x],
+#     %w[- x -],
+#     %w[x - -]
+#   ]
+# }
