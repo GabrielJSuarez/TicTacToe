8c0075352be8d1043f74086ddc4a17b7a1e8fc03
Gabriel Suárez <ginnlaiho@gmail.com>
2020-10-07 16:18:20 -0500

    add test for draw game (true/false) and winner (p1/p2)

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -1,24 +1,65 @@
 # spec/tictactoe_spec.rb
 require './lib/logic'
 
-board = [
-  %w[x o x],
-  %w[o o x],
-  %w[x o x]
-]
-
 describe GameLogic do
-  context 'check if the game is over' do
-    
-    example 'check a draw game' do
-      winner = GameLogic.new
-      expect(winner.check_draw(board, '1', '1', 'x')).to eql(true)
+  subject { 
+    GameLogic.new 
+  }
+  
+  context 'draw game' do
+    let(:draw_board) {
+      board = [
+        ["o", "x", "x"],
+        ["x", "o", "o"],
+        ["o", "x", "x"]
+      ]
+    }
+
+    let(:non_draw_board) {
+      board = [
+        ["o", "-", "x"],
+        ["x", "-", "-"],
+        ["o", "x", "-"]
+      ]
+    }
+
+    example 'draw game' do
+      expect(subject.check_draw(draw_board, '1', '1', 'x')).to be true
     end
 
-    example 'check if there is a winner' do
-      winner = GameLogic.new
-      expect(winner.check_winner(board, '1', '1', 'o')).to eql(false)
+    example 'non draw game' do
+      expect(subject.check_draw(non_draw_board, '1', '1', 'o')).to be false
     end
-    
   end
+
+  context 'winner move' do
+    let(:p1_wins) {
+    [
+      ["x", "x", "x"],
+      ["o", "o", "x"],
+      ["x", "o", "o"]
+    ]
+    }
+
+    let(:p2_wins) {
+    [
+      ["x", "x", "o"],
+      ["x", "o", "x"],
+      ["o", "o", "x"]
+    ]
+    }
+
+    let(:p1_simbol) { 'x' }
+
+    let(:p2_simbol) { 'o' }
+
+    example 'player 1 won' do
+      expect(subject.check_winner(p1_wins, '1', '1', p1_simbol)).to be true
+    end
+
+    example 'player 2 won' do
+      expect(subject.check_winner(p2_wins, '1', '1', p2_simbol)).to be true
+    end
+  end
+  
 end
